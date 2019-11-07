3499844917c687f12773813b8a920bbd26065db9
annie doerfler <anneliesedoerfler@gmail.com>
2019-11-06 17:52:17 -0800

    player move, death, camerasystem

---
+++ PizzaGame/Assets/Player_Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.y < -7){
+			Die ();
+		}
+	}
+		void Die (){ 
+			SceneManager.LoadScene("Prototype_1"); 
+		}
+}
