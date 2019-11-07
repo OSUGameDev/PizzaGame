20013d8b157d73018e8c1ab49a79bed228b44624
powpow26 <powpowtheraichu@gmail.com>
2019-11-06 17:55:45 -0800

    Add Comments/Update Test Scene

--- PizzaGame/Assets/ObjectCreation.cs
+++ PizzaGame/Assets/ObjectCreation.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿/* 
+ * Name: Jordyn Marshall
+ * Date: 11/6/19
+ * Attaching this script to a Game Object should be able to create the same game object
+ * in random positions in the specified room.  
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,12 +18,16 @@ public class ObjectCreation : MonoBehaviour {
 	public int room_x_size;
 	public int room_y_size;
 
+	//Static variable to ensure that no more than the specified objects
+	//are created - prevents recursion. 
 	static int num_created = 1;
 
 	// Use this for initialization
 	void Start () {
-		//Should create objects upon start
+		//If objects need to be created
 		if (num_created < num_objects) {
+			//Gets a random position,  creates an object at that position,
+			//and increates num_created by one. 
 			Vector2 position = get_random_2D_point();
 			Instantiate(this.gameObject, position, transform.rotation);
 			num_created++;
@@ -27,39 +38,11 @@ public class ObjectCreation : MonoBehaviour {
 	void Update () {
 		//Does nothing unless needs to
 	}
-
-	/* Should go through all of the functions to create a new object, as
-	well as ensures that an object isn't created in another object or in an
-	inconvient spot. */
-	public void create_objects() {
-		//bool success, collision;
-
-		for (int i = 0; i < num_objects; i++) {
-			Vector2 position = get_random_2D_point();
-			Instantiate(this.gameObject, position, transform.rotation);
-/* 			success = false;
-
-			while(!success) {
-				Vector2 position = get_random_2D_point();
-				//Check collison
-				//collision = check_collision_with_other_objects();
-				collision = false;
-				//if so, create and set success to true
-				if (!collision) {
-					Instantiate(this_object, position, transform.rotation);
-					success = true;
-
-				}
-				//Otherwise repeat
-			}
-*/		}
-	}
-
+	
 	/* This function should be able to randomly generate an x component and a y component
-	 * for the object to be created. It would take in the room max and min values for
+	 * for the object to be created. It would use the size of the room to determine
 	 * both x and y. 
-	 * - Returns a 2D position vector, object's potential position. 
-	 * COULD EDIT MAX/MIN SO ONLY ONE VARIABLE, SIZE, TO BE DIVIDED BY 2*/
+	 * - Returns a 2D position vector, object's potential position. */
 	public Vector2 get_random_2D_point() {
 		//Randomly generate x position in room, must be within room
 		int x_component = Random.Range(- room_x_size / 2, room_x_size / 2);
@@ -75,17 +58,4 @@ public class ObjectCreation : MonoBehaviour {
 
 	}
 
-	/* Should check if object is in another. Returns false if in another object,
-	returns true otherwise. */
-	public bool check_collision_with_other_objects() {
-		//Check collison - must get info from unity
-		return true;
-	}
-
-	/* Should check if object is close to floor. Returns flase if not, should
-	return true otherwise */
-	public bool check_close_to_floor() {
-		//Check - must get info from unity
-		return true;
-	}
 }
