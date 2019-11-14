902cf0b3b867d8cf389df04a96dffc42400d0427
Samuel-Fox <43558940+Samuel-Fox@users.noreply.github.com>
2019-11-13 17:34:58 -0800

    Update HealthChange.cs
    Added comments to the file

--- PizzaGame/Assets/HealthChange.cs
+++ PizzaGame/Assets/HealthChange.cs
@@ -10,30 +10,31 @@ public class HealthChange : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-       GameObject.Find("GameOver").GetComponent<Text>().enabled = false;
+       GameObject.Find("GameOver").GetComponent<Text>().enabled = false; //Hide the game over message
     }
     void Update()
     {
         if (Input.GetKeyDown("space"))
         {
             health = health - 20;
-            if (health < 0)
-            {
-                health = 0;
-            }
+          //  if (health < 0)
+            //{
+             //   health = 0;
+           // }
             GameObject.Find("HealthNum").GetComponent<UnityEngine.UI.Text>().text = health.ToString();
+            //Change the text component on screen to the health number
         }
 
         if (health == 0)
         {
-            Invoke("restartGame", 5);
-            GameObject.Find("GameOver").GetComponent<Text>().enabled = true;
+            Invoke("restartGame", 5); //Reset the game in 5 seconds
+            GameObject.Find("GameOver").GetComponent<Text>().enabled = true; //Show the game over message
         }
     }
 
     void restartGame ()
     {
-        Application.LoadLevel(Application.loadedLevel);
+        Application.LoadLevel(Application.loadedLevel); //Reload the current level
     }
    
 }
